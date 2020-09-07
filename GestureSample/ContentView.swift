8cb2dd587dc7422e8ff4d8c5e48a60d26ce567b0
QuyetNguyen <quyetnguyentan@gmail.com>
2020-09-07 10:15:50 -0600

    update transient ui state

--- GestureSample/ContentView.swift
+++ GestureSample/ContentView.swift
@@ -12,17 +12,15 @@ struct ContentView: View {
     @GestureState var isDetectingLongPress = false
     var body: some View {
         
-        let tap = TapGesture()
-            .onEnded{_ in
-                print("View tapped")}
-
-        return VStack {
-            Circle()
-                .fill(Color.blue)
-                .frame(width: 100, height: 100, alignment: .center)
-                .gesture(tap)
-            Text("Hello, World!")
+        let press = LongPressGesture(minimumDuration: 1)
+            .updating($isDetectingLongPress) { (currentState, gestureState, transaction) in
+                gestureState = currentState
         }
+        
+        return Circle()
+            .fill(isDetectingLongPress ? Color.yellow : Color.green)
+            .frame(width: 100, height: 100, alignment: .center)
+        .gesture(press)
         
     }
 }
