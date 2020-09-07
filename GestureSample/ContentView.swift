24196540b4a980672fb5caeb21b92a4272031036
QuyetNguyen <quyetnguyentan@gmail.com>
2020-09-07 10:20:43 -0600

    Update permanent state during a gesture

--- GestureSample/ContentView.swift
+++ GestureSample/ContentView.swift
@@ -10,17 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     @GestureState var isDetectingLongPress = false
+    @State var totalNumberOfTaps = 0
     var body: some View {
         
         let press = LongPressGesture(minimumDuration: 1)
             .updating($isDetectingLongPress) { (currentState, gestureState, transaction) in
                 gestureState = currentState
+        }.onChanged { _ in
+            self.totalNumberOfTaps += 1
         }
         
-        return Circle()
-            .fill(isDetectingLongPress ? Color.yellow : Color.green)
-            .frame(width: 100, height: 100, alignment: .center)
-        .gesture(press)
+        return VStack {
+            
+            Text("\(totalNumberOfTaps)")
+                .font(.largeTitle)
+            Circle()
+                .fill(isDetectingLongPress ? Color.yellow : Color.green)
+                .frame(width: 100, height: 100, alignment: .center)
+                .gesture(press)
+        }
         
     }
 }
