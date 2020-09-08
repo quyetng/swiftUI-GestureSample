d6a8bec96a31e0e796527c4159bc65584c19b3fb
QuyetNguyen <quyetnguyentan@gmail.com>
2020-09-07 21:36:10 -0600

    Using TapGesture

--- GestureSample/ContentView.swift
+++ GestureSample/ContentView.swift
@@ -10,9 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
    
+    @State private var tapped = false
+    
+    // create and config the tap gesture
+    var tap: some Gesture {
+        TapGesture(count: 1) // this require 1 tap
+            .onEnded { _ in
+                self.tapped = !self.tapped
+        } // Add action when gesture is ended
+        
+    }
     var body: some View {
         
-      DraggableCircle()
+      // Add tap gesture to the circle
+        
+        Circle().fill(self.tapped ? Color.blue : Color.red)
+            .frame(width: 100, height: 100, alignment: .center)
+        .gesture(tap)
         
     }
 }
@@ -23,76 +37,4 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct DraggableCircle: View {
-    
-    
-    enum DragState {
-        case inactive
-        case pressing
-        case dragging(translation: CGSize)
-        
-        var translation: CGSize {
-            switch self {
-            case DragState.inactive, DragState.pressing:
-                return .zero
-            case DragState.dragging(let translation):
-                return translation
-            }
-        }
-        
-        var isActive: Bool {
-            switch self {
-            case .inactive:
-                return false
-            case .pressing, .dragging:
-                return true
-            }
-        }
-        
-        var isDragging: Bool {
-            switch self {
-            case .inactive, .pressing:
-                return false
-            case .dragging:
-                return true
-            }
-        }
-    }
-    
-    var body: some View {
-        let minimumLongPressDuration = 0.5
-        let longPressDrag = LongPressGesture(minimumDuration: minimumLongPressDuration)
-            .sequenced(before: DragGesture())
-            .updating($dragState) { (value, state, transaction) in
-                switch value {
-                case .first(true):
-                    state = .pressing
-                case .second(true, let drag):
-                    state = .dragging(translation: drag?.translation ?? .zero)
-                default:
-                    state = .inactive
-                }
-                
-        }
-        .onEnded { (value) in
-            guard case .second(true, let drag?) = value else {
-                return
-            }
-            
-            self.viewState.width += drag.translation.width
-            self.viewState.height += drag.translation.height
-        }
-        return Circle().fill(Color.blue)
-            .overlay(dragState.isDragging ? Circle().stroke(Color.white, lineWidth: 2) : nil)
-            .frame(width: 100, height: 100, alignment: .center)
-            .offset(x: viewState.width + dragState.translation.width,
-                    y: viewState.height + dragState.translation.height)
-        .animation(nil)
-            .shadow(radius: dragState.isActive ? 8 : 0)
-            .animation(.linear(duration: minimumLongPressDuration))
-        .gesture(longPressDrag)
-    }
-    
-    @GestureState var dragState = DragState.inactive
-    @State var viewState = CGSize.zero
-}
+
