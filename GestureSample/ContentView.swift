6a6aa4dcd26dd8ab34aa2fd81f04521d5362a927
QuyetNguyen <quyetnguyentan@gmail.com>
2020-09-07 22:01:39 -0600

    Using Drag gesture

--- GestureSample/ContentView.swift
+++ GestureSample/ContentView.swift
@@ -10,29 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
    
-    @GestureState var isDetectingLongPress = false
-    @State var completedLongPress = false
-    
-    // create and config the long press gesture
-    var longPress: some Gesture {
-      LongPressGesture(minimumDuration: 3)
-        .updating($isDetectingLongPress, body: { (currentState, gestureState, transaction) in
-            // update gestureState to currentState
-            gestureState = currentState
-            transaction.animation = Animation.easeIn(duration: 2.0)
+    @GestureState var isDectectDragging = false
+    @State var isDragging = false
+    // create and config the gesture
+    var drag: some Gesture {
+        
+     DragGesture()
+        .onChanged({ (_) in
+            self.isDragging = true
         })
-        .onEnded { (finished) in
-            self.completedLongPress = finished
+        .onEnded { (_) in
+            self.isDragging = false
         }
         
     }
     var body: some View {
         
-      // Add tap gesture to the circle
+      // Add the gesture to the circle
         
-        Circle().fill(self.isDetectingLongPress ? Color.red : (self.completedLongPress ? Color.green : Color.blue))
-            .frame(width: 100, height: 100, alignment: .center)
-        .gesture(longPress)
+       Circle()
+        .fill(self.isDragging ? Color.red : Color.blue)
+        .frame(width: 100, height: 100, alignment: .center)
+        .gesture(drag)
         
     }
 }
