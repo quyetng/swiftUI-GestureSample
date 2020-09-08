f8e2a5d95ac17abacbe545be009f7222dba6e2fb
QuyetNguyen <quyetnguyentan@gmail.com>
2020-09-07 19:36:38 -0600

    Combine gesture to create complex interactions

--- GestureSample/ContentView.swift
+++ GestureSample/ContentView.swift
@@ -9,29 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @GestureState var isDetectingLongPress = false
-    @State var totalNumberOfTaps = 0
-    @State var doneCounting = false
+   
     var body: some View {
         
-        let press = LongPressGesture(minimumDuration: 1)
-            .updating($isDetectingLongPress) { (currentState, gestureState, transaction) in
-                gestureState = currentState
-        }.onChanged { _ in
-            self.totalNumberOfTaps += 1
-        }.onEnded { _ in
-            self.doneCounting = true
-        }
-        
-        return VStack {
-            
-            Text("\(totalNumberOfTaps)")
-                .font(.largeTitle)
-            Circle()
-                .fill(isDetectingLongPress ? Color.yellow : Color.green)
-                .frame(width: 100, height: 100, alignment: .center)
-                .gesture(doneCounting ? nil : press)
-        }
+      DraggableCircle()
         
     }
 }
@@ -40,4 +21,78 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct DraggableCircle: View {
+    
+    
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case DragState.inactive, DragState.pressing:
+                return .zero
+            case DragState.dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isActive: Bool {
+            switch self {
+            case .inactive:
+                return false
+            case .pressing, .dragging:
+                return true
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .inactive, .pressing:
+                return false
+            case .dragging:
+                return true
+            }
+        }
+    }
+    
+    var body: some View {
+        let minimumLongPressDuration = 0.5
+        let longPressDrag = LongPressGesture(minimumDuration: minimumLongPressDuration)
+            .sequenced(before: DragGesture())
+            .updating($dragState) { (value, state, transaction) in
+                switch value {
+                case .first(true):
+                    state = .pressing
+                case .second(true, let drag):
+                    state = .dragging(translation: drag?.translation ?? .zero)
+                default:
+                    state = .inactive
+                }
+                
+        }
+        .onEnded { (value) in
+            guard case .second(true, let drag?) = value else {
+                return
+            }
+            
+            self.viewState.width += drag.translation.width
+            self.viewState.height += drag.translation.height
+        }
+        return Circle().fill(Color.blue)
+            .overlay(dragState.isDragging ? Circle().stroke(Color.white, lineWidth: 2) : nil)
+            .frame(width: 100, height: 100, alignment: .center)
+            .offset(x: viewState.width + dragState.translation.width,
+                    y: viewState.height + dragState.translation.height)
+        .animation(nil)
+            .shadow(radius: dragState.isActive ? 8 : 0)
+            .animation(.linear(duration: minimumLongPressDuration))
+        .gesture(longPressDrag)
+    }
+    
+    @GestureState var dragState = DragState.inactive
+    @State var viewState = CGSize.zero
 }
